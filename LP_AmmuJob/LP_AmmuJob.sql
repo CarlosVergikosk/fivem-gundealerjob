3fc004435c0696059f0e5c3bee1e114cd6ac843d
CarlosVergikosk <45525595+CarlosVergikosk@users.noreply.github.com>
2019-08-02 10:47:43 +0100

    Add files via upload

--- LP_AmmuJob/LP_AmmuJob.sql
+++ LP_AmmuJob/LP_AmmuJob.sql
@@ -14,12 +14,12 @@ INSERT INTO `addon_inventory` (name, label, shared) VALUES
 INSERT INTO `jobs` (`name`, `label`, `whitelisted`) VALUES
 ('ammu', 'Ammu', 1);
 
-INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
-('weapon_piece', 'Weapon Piece', 25, 0, 1);
-
-
 INSERT INTO `job_grades` (`job_name`, `grade`, `name`, `label`, `salary`, `skin_male`, `skin_female`) VALUES
 ('ammu', 0, 'recruit', 'Recruta', 1000, '{}', '{}'),
 ('ammu', 1, 'officer', 'Empregado', 1200, '{}', '{}'),
 ('ammu', 2, 'sergeant', 'Gerente', 1500, '{}', '{}'),
 ('ammu', 3, 'boss', 'Chefe ', 2000, '{\"tshirt_1\":15,\"torso_1\":42,\"shoes\":8,\"pants_1\":24,\"pants_2\":0,\"decals_2\":0,\"hair_color_2\":0,\"face\":19,\"helmet_2\":0,\"hair_2\":0,\"arms\":8,\"torso_2\":0,\"hair_color_1\":5,\"hair_1\":2,\"skin\":34,\"sex\":0,\"glasses_1\":0,\"glasses_2\":1,\"decals_1\":0,\"glasses\":0,\"tshirt_2\":0,\"helmet_1\":-1}', '{}');
+
+INSERT INTO `items` (name, label, `limit`) VALUES
+	('weapon_piece','Weapon Piece', 25)
+;
